df65d070a86d2f16e4738f8cc12017a2da3bc285
Lua <euluarodrigues@gmail.com>
2024-07-24 11:44:54 -0300

    pequena modificação

--- Mod13/TarefaClasseAbstrata/src/br/com/lua/principal/Main.java
+++ Mod13/TarefaClasseAbstrata/src/br/com/lua/principal/Main.java
@@ -1,22 +1,21 @@
 package br.com.lua.principal;
 
-import br.com.lua.modelo.Pessoa;
 import br.com.lua.modelo.PessoaFisica;
 import br.com.lua.modelo.PessoaJuridica;
 
 public class Main {
     public static void main(String[] args) {
-        PessoaFisica pessoa = new PessoaFisica();
-        pessoa.setNome("Maria");
-        pessoa.setEndereco("Rua A, 55");
-        pessoa.setCpf(123456789L);
+        PessoaFisica pessoa1 = new PessoaFisica();
+        pessoa1.setNome("Maria");
+        pessoa1.setEndereco("Rua A, 55");
+        pessoa1.setCpf(123456789L);
 
-        PessoaJuridica empresa = new PessoaJuridica();
-        empresa.setNome("Embratel");
-        empresa.setEndereco("Av. b, 103");
-        empresa.setCnpj(555001L);
+        PessoaJuridica empresa1 = new PessoaJuridica();
+        empresa1.setNome("Embratel");
+        empresa1.setEndereco("Av. b, 103");
+        empresa1.setCnpj(555001L);
 
-        pessoa.imprimePessoa(pessoa);
-        empresa.imprimePessoa(empresa);
+        pessoa1.imprimePessoa(pessoa1);
+        empresa1.imprimePessoa(empresa1);
     }
 }
